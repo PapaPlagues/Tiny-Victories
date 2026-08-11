cf09b95e7bd7cfe2a302b3c9ac7348d2bd50023f
Skylord Jacob <189800964+PapaPlagues@users.noreply.github.com>
2026-08-11 17:55:50 -0400

    Improve card css

--- public-client/src/components/Card.jsx
+++ public-client/src/components/Card.jsx
@@ -2,33 +2,54 @@ import "../styles/Card.css";
 import placeholderImage from "../assets/placeholder.png";
 import Tag from "./Tag";
 
-function Card({ title, excerpt, img, link, tags, onTagClick}) {
+function Card({ title, excerpt, img, link, tags, onTagClick }) {
 
     const tagList = tags || [];
 
-    return(
+    return (
         <a className="card" href={link}>
+
             {img ? (
-                <img src={img} alt={title} className="card-img" />
+                <img
+                    src={img}
+                    alt={title}
+                    className="card-img"
+                />
             ) : (
-                <img src={placeholderImage} alt={title} className="card-img" />
+                <img
+                    src={placeholderImage}
+                    alt={title}
+                    className="card-img"
+                />
             )}
 
             <div className="card-content">
+
                 <h2>{title}</h2>
+
                 <p>{excerpt}</p>
+
             </div>
 
-            <div className="card-tags">
-                {tagList.map((tag, index) => (
-                    <Tag key={index} onClick={onTagClick}>
-                        {typeof tag === "string" ? tag : tag.name}
-                    </Tag>
-                ))}
-            </div>
+            {tagList.length > 0 && (
+                <div className="card-tags">
+
+                    {tagList.map((tag, index) => (
+                        <Tag
+                            key={index}
+                            onClick={onTagClick}
+                        >
+                            {typeof tag === "string"
+                                ? tag
+                                : tag.name}
+                        </Tag>
+                    ))}
+
+                </div>
+            )}
+
         </a>
     );
 }
-
 
 export default Card;
